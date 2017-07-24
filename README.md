2c6885bb3698f725a7542e7fb22f43a02c8669c3
neugebauer@cs.uni-duesseldorf.de <neugebauer@cs.uni-duesseldorf.de>
2017-07-24 15:31:39 +0200

    Include small example D-BAS mockup web app

--- README.md
+++ README.md
@@ -13,11 +13,14 @@ To run the service, execute:
     make run
     
 This module requires a running D-BAS instance on localhost.
-Alternatively, provide the D-BAS export interface yourself and serve the json
-export for the graph data and user opinion data at (respectively):
+Alternatively, you can provide the D-BAS export interface yourself and serve the json export for the graph data and user opinion data at (respectively):
 
     http://localhost:4284/export/doj/<discussion_id>
     http://localhost:4284/export/doj_user/<user_id>/<discussion_id>
+    
+A small python web app that serves example D-BAS data is included. To run it, execute:
+
+    python3 dbas_export_mockup.py
     
 ## Dung AF Interface
 
---
+++ dbas_export_mockup.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Set security headers for Web requests
+
+
+@app.route('/export/doj/<int:discussion>')
+def export_dummy_discussion(discussion):
+    """
+    Return a json string with dbas export style data of an example discussion.
+
+    :param discussion: discussion ID
+    :return: json string
+    """
+
+    nodes = []
+    inferences = []
+    undercuts = []
+
+    if discussion == 1:
+        # Very small and simple demo discussion
+        nodes = [1, 2, 3]
+        inferences = [{'id': 1,
+                       'premises': [2],
+                       'is_supportive': True,
+                       'conclusion': 1},
+                      {'id': 2,
+                       'premises': [3],
+                       'is_supportive': False,
+                       'conclusion': 1}]
+
+    if discussion == 2:
+        # Medium demo discussion
+        nodes = [76, 77, 78, 79, 80, 81, 82]
+        inferences = [{'id': 65,
+                       'premises': [77],
+                       'is_supportive': True,
+                       'conclusion': 76},
+                      {'id': 66,
+                       'premises': [78],
+                       'is_supportive': False,
+                       'conclusion': 76},
+                      {'id': 67,
+                       'premises': [79],
+                       'is_supportive': False,
+                       'conclusion': 76},
+                      {'id': 68,
+                       'premises': [80, 81],
+                       'is_supportive': False,
+                       'conclusion': 79}]
+        undercuts = [{'id': 69,
+                      'premises': [82],
+                      'conclusion': 65}]
+
+    return jsonify({'nodes': nodes,
+                    'inferences': inferences,
+                    'undercuts': undercuts})
+
+
+@app.route('/export/doj_user/<int:user>/<int:discussion>')
+def export_dummy_useropinion(user, discussion):
+    """
+    Return a json string with dbas export style data of an example discussion.
+
+    :param user: user ID
+    :param discussion: discussion ID
+    :return: json string
+    """
+
+    marked_statements = []
+    marked_arguments = []
+    rejected_arguments = []
+    accepted_statements_via_click = []
+    rejected_statements_via_click = []
+
+    if discussion == 1:
+        if user == 1:
+            accepted_statements_via_click = [1]
+            rejected_statements_via_click = [3]
+
+    elif discussion == 2:
+        if user == 1:
+            accepted_statements_via_click = [76, 77, 80, 81]
+            rejected_statements_via_click = [78, 79, 82]
+
+    return jsonify({'marked_statements': marked_statements,
+                    'marked_arguments': marked_arguments,
+                    'rejected_arguments': rejected_arguments,
+                    'accepted_statements_via_click': accepted_statements_via_click,
+                    'rejected_statements_via_click': rejected_statements_via_click})
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=4284)
