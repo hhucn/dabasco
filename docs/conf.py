9ecacf567f8164856dbb024ab09a3d3f8a29785f
Daniel Neugebauer <neugebauer@cs.uni-duesseldorf.de>
2017-11-30 14:35:10 +0100

    Really add code coverage xml, fix more pep8

--- docs/conf.py
+++ docs/conf.py
@@ -36,11 +36,11 @@ sys.path.insert(0, os.path.abspath('../dabasco/aspic'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.imgmath',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
