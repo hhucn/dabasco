4b236c6c06ce70e0fbb494e30d8fe208bf1fd613
Daniel Neugebauer <neugebauer@cs.uni-duesseldorf.de>
2018-08-03 17:44:57 +0200

    More unit testing for ADF import from D-BAS

--- dabasco/adf/tests/test_import.py
+++ dabasco/adf/tests/test_import.py
@@ -16,9 +16,11 @@ logger = logging.getLogger('test')
 
 class TestADFImport(unittest.TestCase):
 
-    def test_1statement_test1(self):
+    def test_discussion1_user1_defeasible(self):
         discussion_id = 1
         user_id = 1
+        strict_opinion = False
+
         dbas_discussion_json = {
             "inferences": [{"conclusion": 1, "id": 1, "is_supportive": True, "premises": [2]},
                            {"conclusion": 1, "id": 2, "is_supportive": False, "premises": [3]}],
@@ -32,9 +34,10 @@ class TestADFImport(unittest.TestCase):
             "rejected_arguments": [],
             "rejected_statements_via_click": [3],
         }
+
         dbas_discussion = import_dbas_graph(discussion_id=discussion_id, graph_export=dbas_discussion_json)
         dbas_user = import_dbas_user(discussion_id=discussion_id, user_id=user_id, user_export=dbas_user_json)
-        adf_result = import_adf(dbas_discussion, dbas_user, False)
+        adf_result = import_adf(dbas_discussion, dbas_user, strict_opinion)
 
         adf_reference = ADF()
 
@@ -63,6 +66,73 @@ class TestADFImport(unittest.TestCase):
             ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 's3')),
             ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'na3'))]))
         adf_reference.add_statement('na3', ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'a3')))
+
+        # D-BAS Inferences
+        adf_reference.add_statement('i1', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'ns1')),
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'ni1')),
+            ADFNode(ADFNode.LEAF, 's2')]))
+        adf_reference.add_statement('ni1', ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'i1')))
+        adf_reference.add_statement('i2', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 's1')),
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'ni2')),
+            ADFNode(ADFNode.LEAF, 's3')]))
+        adf_reference.add_statement('ni2', ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'i2')))
+
+        self.assertTrue(adf_reference.is_equivalent_to(adf_result))
+
+    def test_discussion1_user1_strict(self):
+        discussion_id = 1
+        user_id = 1
+        strict_opinion = True
+
+        dbas_discussion_json = {
+            "inferences": [{"conclusion": 1, "id": 1, "is_supportive": True, "premises": [2]},
+                           {"conclusion": 1, "id": 2, "is_supportive": False, "premises": [3]}],
+            "nodes": [1, 2, 3],
+            "undercuts": []
+        }
+        dbas_user_json = {
+            "accepted_statements_via_click": [2],
+            "marked_arguments": [],
+            "marked_statements": [],
+            "rejected_arguments": [],
+            "rejected_statements_via_click": [3],
+        }
+
+        dbas_discussion = import_dbas_graph(discussion_id=discussion_id, graph_export=dbas_discussion_json)
+        dbas_user = import_dbas_user(discussion_id=discussion_id, user_id=user_id, user_export=dbas_user_json)
+        adf_result = import_adf(dbas_discussion, dbas_user, strict_opinion)
+
+        adf_reference = ADF()
+
+        # D-BAS Statements
+        adf_reference.add_statement('s1', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'ns1')),
+            ADFNode(ADFNode.OR, ADFNode(ADFNode.LEAF, 'i1'))]))
+        adf_reference.add_statement('ns1', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 's1')),
+            ADFNode(ADFNode.OR, ADFNode(ADFNode.LEAF, 'i2'))]))
+        adf_reference.add_statement('s2', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'ns2')),
+            ADFNode(ADFNode.OR, ADFNode(ADFNode.LEAF, 'a2'))]))
+        adf_reference.add_statement('ns2', ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_FALSE))
+        adf_reference.add_statement('s3', ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_FALSE))
+        adf_reference.add_statement('ns3', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 's3')),
+            ADFNode(ADFNode.OR, ADFNode(ADFNode.LEAF, 'a3'))]))
+
+        # User opinion
+        adf_reference.add_statement('a2', ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_TRUE))
+        adf_reference.add_statement('na2', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 's2')),
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'na2'))
+        ]))
+        adf_reference.add_statement('a3', ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_TRUE))
+        adf_reference.add_statement('na3', ADFNode(ADFNode.AND, [
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'ns3')),
+            ADFNode(ADFNode.NOT, ADFNode(ADFNode.LEAF, 'na3'))
+        ]))
 
         # D-BAS Inferences
         adf_reference.add_statement('i1', ADFNode(ADFNode.AND, [
