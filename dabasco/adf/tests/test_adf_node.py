b3c88320ad1bd49a57a97ebb4b5d919a958fc2e5
Daniel Neugebauer <neugebauer@cs.uni-duesseldorf.de>
2018-08-07 11:03:43 +0200

    Add more tests for ADFNode

--- dabasco/adf/tests/test_adf_node.py
+++ dabasco/adf/tests/test_adf_node.py
@@ -13,35 +13,42 @@ logger = logging.getLogger('test')
 
 class TestADFNode(unittest.TestCase):
 
-    def test_constructor1(self):
+    def test_equivalence1(self):
         node1 = ADFNode(ADFNode.LEAF, 'name')
         node2 = ADFNode(ADFNode.LEAF, ['name'])
 
         self.assertTrue(node1.is_equivalent_to(node2))
 
-    def test_constructor2(self):
+    def test_equivalence2(self):
         node1 = ADFNode(ADFNode.LEAF, 'name')
         node2 = ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_TRUE)
 
         self.assertFalse(node1.is_equivalent_to(node2))
 
-    def test_constructor3(self):
+    def test_equivalence3(self):
         node1 = ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_FALSE)
         node2 = ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_TRUE)
 
         self.assertFalse(node1.is_equivalent_to(node2))
 
-    def test_constructor4(self):
+    def test_equivalence4(self):
         node0 = ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_FALSE)
         node1 = ADFNode(ADFNode.NOT, node0)
         node2 = ADFNode(ADFNode.NOT, [node0])
 
         self.assertTrue(node1.is_equivalent_to(node2))
 
-    def test_constructor5(self):
+    def test_equivalence5(self):
         node0 = ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_FALSE)
         node1 = ADFNode(ADFNode.NOT, node0)
         node2 = ADFNode(ADFNode.NOT, [node0, node0])
+
+        self.assertFalse(node1.is_equivalent_to(node2))
+
+    def test_equivalence6(self):
+        node0 = ADFNode(ADFNode.LEAF, ADFNode.CONSTANT_FALSE)
+        node1 = ADFNode(ADFNode.NOT, node0)
+        node2 = ADFNode(ADFNode.NOT, ADFNode.CONSTANT_FALSE)
 
         self.assertFalse(node1.is_equivalent_to(node2))
 
